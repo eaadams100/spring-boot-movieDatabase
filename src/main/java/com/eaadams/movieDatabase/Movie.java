133033012e238edcca95d444d3d3c2d3282ae0a5
A. Emmanuel Adams <eaadams777@gmail.com>
2025-10-11 08:31:38 +0000

    spring boot movie database

---
+++ src/main/java/com/eaadams/movieDatabase/Movie.java
@@ -0,0 +1,41 @@
+package com.eaadams.movieDatabase;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.util.List;
+
+@Document(collection = "movies")
+@Data // takes care of all getters and setters
+@AllArgsConstructor // creating a constructor that takes all the private field as arguments
+@NoArgsConstructor // another constructor that takes no parameters whatsoever
+public class Movie {
+
+    @Id
+    private ObjectId id;
+
+    private String imdbId;
+
+    private String title;
+
+    private String releaseDate;
+
+    private String trailerLink;
+
+    private String poster;
+
+    private List<String> genres;
+
+    private List<String> backdrops;
+
+    @DocumentReference
+    private List<Review> reviewIds;
+
+
+
+}
---
+++ src/test/java/com/eaadams/movieDatabase/MovieDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eaadams.movieDatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
